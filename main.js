afaedba5c7c2a36f577924d48abb0f126b13ce76
Kelly DeMent <kdement@ufl.edu>
2019-10-19 00:24:09 -0600

    Fix dropdown interaction

--- main.js
+++ main.js
@@ -2,18 +2,33 @@
 var accordionHeaders = document.querySelectorAll('#accordion .accordion-headers');
 var accordionDescriptions = document.querySelectorAll('#accordion .accordion-description');
 
+
 //Handles the accordion dropdown action
 function expandAccordionDescription () {
 
-  //Handles toggle indicator
-  for (i=0; i<accordionHeaders.length; i++) {
-    accordionHeaders[i].childNodes[3].innerHTML = '+';
-    accordionDescriptions[i].classList.remove('accordion-open');
+  var box = this.nextElementSibling;
+  var toggleIndicator = this.childNodes[3];
+
+  //If box is open, close box on click and change indicator
+  if (box.classList.contains('active')) {
+    box.classList.remove('active');
+    toggleIndicator.innerHTML = '+';
   }
-  this.nextElementSibling.classList.add('accordion-open');
-  this.childNodes[3].innerHTML = '-';
+
+  //If box is closed, close all boxes on click and change indicator
+  else {
+    for (var i = 0; i < accordionHeaders.length; i++) {
+      accordionDescriptions[i].classList.remove('active');
+      accordionHeaders[i].childNodes[3].innerHTML = '+';
+    }
+
+  //Then open clicked box and change indicator
+    box.classList.add('active');
+    toggleIndicator.innerHTML = '-';
+  }
 
 }
+
 
 //Add cick event listener for accordion headers
 for (i=0; i<accordionHeaders.length; i++) {
